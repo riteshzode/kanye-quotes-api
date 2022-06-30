8e3ce93f9cf15ae748a2c096182fba05066c2f4e
Ritesh Zode <106472286+riteshzode@users.noreply.github.com>
2022-06-30 20:24:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def get_quote():
     canvas.itemconfig(quote_text, text=data)
 
 
+# design os app
+
 window = Tk()
 window.title("Kanye Says...")
 window.config(padx=50, pady=50)
@@ -18,12 +20,12 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 20, "bold"),
-                                fill="white")
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 20, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
+# loop 
 window.mainloop()
